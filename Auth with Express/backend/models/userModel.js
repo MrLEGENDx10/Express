053fa46eb85f5050469278164f2655dfda3941f7
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-08 01:59:56 +0530

    g

--- Auth with Express/backend/models/userModel.js
+++ Auth with Express/backend/models/userModel.js
@@ -1,24 +1,29 @@
 const mongoose =require ('mongoose');
+const {Schema}=mongoose;
 
-const userSchema= new mongoose.Schema({
-//     name:{
-//         type:String,
-//         required:true,
-//         trim:true
-//     },
-//     email:{
-//         type:String,
-//         required:true,
-//         unique:true,
-//         trim:true
-//     },
-//     password:{
-//         type:String,
-//         required:true,
-//         trim:true
-//     }
-// },{
-//     timestamps:true
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:[true, "Name is required"],
+        minlength:[3,"Name must be at least 3 characters long"],
+        maxlength:[20,"Name must be less than 20 characters long"],
+        trim:true
+    },
+    email:{
+        type:String,
+        required:[true, "Email is required"],
+        lowercase:true,
+        unique:[true,"Email already exists"]
+    },
+    password:{
+        type:String,
+        required:[true, "Password is required"],
+        minlength:[6,"Password must be at least 6 characters long"]
+    },
+    forgetPasswordToken:{
+        type:String
+    }
 })
 
-module.exports=mongoose.model("User",userSchema);
+const userModel=mongoose.model("User",userSchema);
+module.exports=userModel;
