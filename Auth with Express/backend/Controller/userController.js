fe92b09f9b077a922ac236e843ad1d08bffb626a
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-08 16:47:49 +0530

    usercontroller

--- Auth with Express/backend/Controller/userController.js
+++ Auth with Express/backend/Controller/userController.js
@@ -1,5 +1,28 @@
-const User = require('../models/userModel');
+const userModel = require('../models/userModel');
+
 
 exports.home = async (req,res)=>{
     res.send('Home Route');
 }
+
+exports.signUp = async (req,res) =>{
+    const { name,email,password,ConfirmPassword } = req.body;
+    try {
+        const userInfo = new userModel({ name,email,password,ConfirmPassword });
+        const result = await userInfo.save();
+        return res.status(200).json({
+            success:true,
+            data: result
+        })
+    } catch (error) {
+        if(error.code === 11000){
+            return res.status(400).json({
+                status:"fail",
+                message:"Email already exists"
+            })}
+        console.log(error);
+        res.status(400).json({
+            status:"fail",
+            message: error.message
+        })
+}}
