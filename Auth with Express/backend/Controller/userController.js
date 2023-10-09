b5efb747278aaf00543c5a0f52dbc7276b90332a
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-09 23:11:36 +0530

    m

--- Auth with Express/backend/Controller/userController.js
+++ Auth with Express/backend/Controller/userController.js
@@ -1,19 +1,38 @@
 const userModel = require('../models/userModel');
+const emailValidator = require('email-validator');
 
 
-exports.home = async (req,res)=>{
+const home = async (req,res)=>{
     res.send('Home Route');
 }
 
-exports.signUp = async (req,res) =>{
+const signUp = async (req,res) =>{
     const { name,email,password,ConfirmPassword } = req.body;
     try {
-        const userInfo = new userModel({
-            name,
-            email,
-            password,
-            ConfirmPassword
-        });
+            if(!name || !email || !password || !ConfirmPassword){
+                return res.status(400).json({
+                    status:"fail",
+                    message:"All fields are required"
+                })
+            }
+
+        var validEmail = emailValidator.validate(email);
+        if(!validEmail){
+            return res.status(400).json({
+                status:"fail",
+                message:"Invalid Email"
+            })
+        }
+
+        if(password != ConfirmPassword){
+            return res.status(400).json({
+                status:"fail",
+                message:"Passwords do not match"
+            })
+        }
+
+
+        const userInfo = new userModel(req.body);
         const result = await userInfo.save();
         return res.status(200).json({
             success:true,
@@ -31,3 +50,54 @@ exports.signUp = async (req,res) =>{
             message: error.message
         })
 }}
+
+const signIn = async (req,res)=>{
+    const {email,password} = req.body;
+    try {
+        if(!email || !password){
+            return res.status(400).json({
+                status:"fail",
+                message:"All fields are required"
+            })
+        }
+
+        var validEmail = emailValidator.validate(email);
+        if(!validEmail){
+            return res.status(400).json({
+                status:"fail",
+                message:"Invalid Email"
+            })
+        }
+
+        const result = await userModel.findOne({email:email});
+        if(!result){
+            return res.status(400).json({
+                status:"fail",
+                message:"Email does not exist"
+            })
+        }
+
+        if(password != result.password){
+            return res.status(400).json({
+                status:"fail",
+                message:"Invalid Password"
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            data: result
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: error.message
+        })
+    }
+}
+
+module.exports = {
+    home,
+    signUp,
+    signIn
+}
