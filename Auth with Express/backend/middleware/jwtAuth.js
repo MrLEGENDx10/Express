1889707f4bd66e5a45ca389af87a2e01c615b7a1
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-11 00:48:00 +0530

    jwt auth

---
+++ Auth with Express/backend/middleware/jwtAuth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const jwtAuth =(req,res,next)=>{
+    // verify token 
+    // inject user info in req 
+
+    const token = (req.cookies && req.cookies.token) || null;
+    if(!token){
+        return res.status(400).json({
+            success:false,
+            message : "NOT authorized"
+        })
+    }
+    try {
+        const payload = jwt.verify(token,process.env.SECRET);
+        
+    } catch (error) {
+        return res.status(400).json({
+            success:false,
+            message: error.message
+        })
+    }
+
+    next();
+}
+
+module.exports = jwtAuth;
--- Auth with Express/backend/routes/userRoutes.js
+++ Auth with Express/backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const {home , signUp,signIn,getUser} = require('../Controller/userController.js');
+const jwtAuth = require('../middleware/jwtAuth.js')
 
 const router =express.Router();
 
 router.get('/',home);
 router.post('/signup',signUp)
 router.post('/signin',signIn)
-router.get('/user',getUser)
+router.get('/user',jwtAuth,getUser)
 
 
 module.exports = router;
