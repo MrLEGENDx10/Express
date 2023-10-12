9cca442740e4e0f8fec5b3ba530f1ce0b46e036e
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-12 22:02:20 +0530

    CORS

--- Auth with Express/backend/app.js
+++ Auth with Express/backend/app.js
@@ -8,14 +8,19 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config(
     { path: './.env' }
 )
-const bcrypt= require('bcryptjs');
+
 
 connectToDb();
 
 app.use(express.json())
 app.use(cookieParser()); // to parse the cookie
 app.use(express.urlencoded({extended:true}))
-app.use(cors());
+app.use(cors(
+    {
+        origin:[process.env.CLIENT_URL],
+        credentials:true
+    }
+));
 
 app.use(express.urlencoded({ extended: true }))
 const userRoutes = require('./routes/userRoutes.js')
